31ca7cda47a9263dcf5639081f1ae5952126473f
Reaster <earnaud@student.42.fr>
2022-02-28 19:05:34 +0100

    added a small main of tests x:

--- main.cpp
+++ main.cpp
@@ -4,229 +4,37 @@
 #include "stack.hpp"
 #include "rb_tree.hpp"
 #include "map.hpp"
-#include <map>
 #include <fstream>
-#include <vector>
-#include <list>
-#include <string>
-
-# define _END			"\x1b[0m"
-# define _BOLD			"\x1b[1m"
-# define _UNDER			"\x1b[4m"
-# define _REV			"\x1b[7m"
-
-/*
-** Colors
-*/
-# define _GREY			"\x1b[30m"
-# define _RED			"\x1b[31m"
-# define _GREEN			"\x1b[32m"
-# define _YELLOW		"\x1b[33m"
-# define _BLUE			"\x1b[34m"
-# define _PURPLE		"\x1b[35m"
-# define _CYAN			"\x1b[36m"
-# define _WHITE			"\x1b[37m"
-
-/*
-** Inverted, i.e. colored backgrounds
-*/
-# define _IGREY			"\x1b[40m"
-# define _IRED			"\x1b[41m"
-# define _IGREEN		"\x1b[42m"
-# define _IYELLOW		"\x1b[43m"
-# define _IBLUE			"\x1b[44m"
-# define _IPURPLE		"\x1b[45m"
-# define _ICYAN			"\x1b[46m"
-# define _IWHITE		"\x1b[47m"
-#include <iomanip>
-
-template <class Key, class T>
-void	print(ft::map<Key, T>& lst)
-{
-	std::cout << "printing a map : \n";
-	for (typename ft::map<Key, T>::iterator it = lst.begin(); it != lst.end(); it++)
-		std::cout << it->first << " => " << it->second << '\n';
-}
 
 int	main(void)
 {
-	std::cout << _WHITE << "# test_map" << _END << std::endl;
-	std::cout << _YELLOW <<  "/* ********************************************************************** */" << std::endl;
-	std::cout << "/*                          "<< _WHITE << "BASIC TESTS" << _YELLOW << "                                   */" << std::endl;
-	std::cout << "/* ********************************************************************** */" << _END << std::endl;
-	std::cout << std::endl;
+	ft::map<int, std::string> ma_map;
 
+	ma_map.insert(ft::pair<int, std::string>(30, "lol"));
+	ma_map[64] = "ceci est une phrase";
+	ma_map[42] = "ce projet etait long!";
+	ma_map[1] = "et on enleve une node?";
+	ma_map.print();
 
-	ft::map<std::string, int> mymap;
-	print(mymap);
+	std::cout << "---------------------------------------" << std::endl;
+	ma_map.erase(42);
+	ma_map.print();
 
+	ft::vector<int> vectour;
 
-	std::cout << "myft::map<" << _PURPLE << "std::string" << _END << ", " << _PURPLE << "int" << _END <<"> mymap;" << std::endl;
+	vectour.push_back(42);
+	vectour[1] = 50;
+	std::cout << "vectour[0] =" << vectour[0] << " and vectour[1] =" << vectour[1] << std::endl;
 
-	std::cout << std::endl;
-	std::cout << "mymap.max_size(): " << (mymap.max_size() > 10000) << std::endl;
-	print(mymap);
-	std::cout << std::endl;
+	ft::stack<int> stick;
 
-
-	print(mymap);
-	mymap.insert(ft::pair<std::string, int>("ft", 42));
-
-	print(mymap);
-	mymap.insert(ft::pair<std::string, int>("one", 1));
-
-
-	std::cout << _WHITE << "# testing upper/lower_bound" << _END << std::endl;
-	ft::map<std::string, int>::iterator mit;
-	ft::map<std::string, int>::iterator it;
-	print(mymap);
-	it = mymap.lower_bound("aaa");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.lower_bound(\"aaa\"): " << it->first << ":" << it->second << std::endl;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.lower_bound("one");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.lower_bound(\"one\"): " << it->first << ":" << it->second << std::endl;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.lower_bound("oae");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.lower_bound(\"oae\"): " << it->first << ":" << it->second << std::endl;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.lower_bound("ft");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.lower_bound(\"ft\"): " << it->first << ":" << it->second << std::endl;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.lower_bound("zzz");
-	print(mymap);
+	stick.push(42);
+	stick.push(50);
+	std::cout << "stick top = " << stick.top() << " and remember that stack is a fraud of vector" << std::endl;
 	
-	if (it == mymap.end())
-		std::cout << std::setw(40) << "mymap.lower_bound(\"zzz\"): mymap.end()" << std::endl;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.upper_bound("aaa");
-	
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.upper_bound(\"aaa\"): " << it->first << ":" << it->second << std::endl;;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.upper_bound("oae");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.upper_bound(\"oae\"): " << it->first << ":" << it->second << std::endl;;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.upper_bound("one");
-	
-	if (it == mymap.end())
-		std::cout << std::setw(40) << "mymap.upper_bound(\"one\"): mymap.end()" << std::endl;
-	print(mymap);
-	std::cout << std::endl;
-	it = mymap.upper_bound("ft");
-	print(mymap);
-	std::cout << std::setw(40) << "mymap.upper_bound(\"ft\"): " << it->first << ":" << it->second << std::endl;;
-	std::cout << std::endl;
-	print(mymap);
-	it = mymap.upper_bound("zzz");
-	
-	if (it == mymap.end())
-		std::cout << std::setw(40) << "mymap.upper_bound(\"zzz\"): mymap.end()" << std::endl;
-	print(mymap);
-	std::cout << std::endl;
-
-	print(mymap);
-	mymap.erase("undefined");
-	print(mymap);
-	mymap.erase("ft");
-	print(mymap);
-	std::cout << _WHITE << "# double clear" << _END << std::endl;
-	mymap.clear();
-	print(mymap);
-	mymap.clear();
-	print(mymap);
-	mymap["lol"];
-	print(mymap);
-	mymap.insert(ft::pair<std::string, int>("xD", 123));
-	print(mymap);
-	mymap.insert(mymap.end(), ft::pair<std::string, int>("uch", 442));
-	print(mymap);
-	mymap.insert(mymap.begin(), ft::pair<std::string, int>("uch", 22));
-	print(mymap);
-	mymap.insert(mymap.end(), ft::pair<std::string, int>("uch", 23));
-	print(mymap);
-	mymap.insert(ft::pair<std::string, int>("uch", 23));
-	print(mymap);
-	mymap["lol"] = 8;
 
 
-	print(mymap);
-	ft::map<std::string, int> map2;
-
-	print(mymap);
-	print(map2);
-	std::cout << "myft::map<" << _PURPLE << "std::string" << _END << ", " << _PURPLE << "int" << _END <<"> mymap2;" << std::endl;
-	print(mymap);
-	std::cout << "empty line\n";
-	print(map2);
-	std::cout << "empty line2\n";
-
-	swap(mymap, map2);
-	std::cout << "empty line3\n";
-	print(mymap);
-	std::cout << "empty line4\n";
-	print(map2);
-	std::cout << "empty line5\n";
-
-	mymap = map2;
-	print(mymap);
-	print(map2);
-
-	std::cout << std::setw(40) << "map == map2: " << (mymap == map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
-
-	mymap.clear();
-	print(mymap);
-	print(map2);
-
-	mymap["satan"] = 666;
-	print(mymap);
-	print(map2);
-	std::cout << std::setw(40) << "mymap == map2: " << (mymap == map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
-	std::cout << std::setw(40) << "mymap <= map2: " << (mymap <= map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
-	std::cout << std::setw(40) << "mymap >= map2: " << (mymap >= map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
-	std::cout << std::setw(40) << "mymap > map2: " << (mymap > map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
-	std::cout << std::setw(40) << "mymap < map2: " << (mymap > map2) << std::endl;
-	print(mymap);
-	print(map2);
-	std::cout << std::endl;
+	std::cout << "\nplease use mli tester https://github.com/mli42/containers_test.git or mazoise https://github.com/Mazoise/42TESTERS-CONTAINERS.git tester for further tests for the correction" << std::endl;
 	
 	return 0;
-
 }
-
-    // std::ifstream source("test", std::ios::binary);
-    // std::ft::ft::vector<char> data((std::istreambuf_iterator<char>(source)),
-    //                    std::istreambuf_iterator<char>());
-    // std::std::std::std::std::cout << data[0] << data[1] << data[2] << std::endl;
-    // source.close();
-    // std::ifstream sourceb("test", std::ios::binary);
-    // ft::ft::ft::vector<char> data2((std::istreambuf_iterator<char>(sourceb)),
-    //                    std::istreambuf_iterator<char>());
-    // std::std::std::std::std::cout << data2[0] << data2[1] << data2[2] << std::endl;
-    // return(0);
